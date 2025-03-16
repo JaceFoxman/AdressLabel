226555841c7aa9960fb95e22c65e960b74ae2241
Jason Permann <JaceFoxman@gmail.com>
2025-03-16 15:06:10 -0600

    Rename and add header

---
+++ AdressLabel/AddressLabelForm.vb
@@ -0,0 +1,14 @@
+﻿'Jason Permann
+'Spring 2025
+'RCET2265
+'Adress label
+'https://github.com/JaceFoxman/AdressLabel.git
+
+Option Compare Text
+Option Explicit On
+Option Strict On
+Public Class AddressLabelForm
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
